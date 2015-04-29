045fd15db7fdaea0e62b24d4079dbb596b63e983
Sean M. Collins <sean@coreitpro.com>
2015-04-28 21:36:20 -0400

    CLI tool

---
+++ philly-bike-router/cli.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+
+API_ENDPOINT = "https://api.phila.gov/bike-share-stations/v1/"
+
+def main():
+    r = requests.get(API_ENDPOINT, headers={'User-Agent': 'curl/7.37.1'})
+    data = json.loads(r.content)
+
+    for station in data['features']:
+        if station['properties']['docksAvailable'] < 3:
+            print("%s is nearly full - %d spaces left" %
+                  (station['properties']['name'].strip(),
+                   station['properties']['docksAvailable']))
+
+if __name__ == "__main__":
+    main()
