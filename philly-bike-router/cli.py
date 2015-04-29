826a2ef88db1c419c0a631e3157b93828f0a18ea
Sean M. Collins <sean@coreitpro.com>
2015-04-28 22:31:39 -0400

    Report on bikes available

--- philly-bike-router/cli.py
+++ philly-bike-router/cli.py
@@ -10,14 +10,20 @@ def main():
 
     for station in data['features']:
         spaces = station['properties']['docksAvailable']
+        available = station['properties']['bikesAvailable']
         name = station['properties']['name'].strip()
         if spaces == 0:
             print("%s is full" % name)
-        elif spaces < 3:
+            continue
+        if not available:
+            print("%s has no bikes" % name)
+            continue
+        if spaces < 3:
             plural = "space" if spaces == 1 else "spaces"
             print("%s is nearly full - %d %s left" % (name, spaces, plural))
-        if not station['properties']['bikesAvailable']:
-            print("%s has no bikes" % name)
+        if available < 3:
+            plural = "bike" if available == 1 else "bikes"
+            print("%s is nearly empty - %d %s left" % (name, available, plural))
 
 if __name__ == "__main__":
     main()
